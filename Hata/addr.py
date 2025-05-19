7f04baf718f4207941c845ed1da4f14a2cd9ef57
unikof <unikof@hotmail.com>
2025-05-19 15:45:56 +0300

    2

--- Hata/addr.py
+++ Hata/addr.py
@@ -1,2 +1,12 @@
-detsk_light_potolok = b'\xC0\x5D\x89\xB0\x9C\xB8'
-detsk_remote_stena = b'\x34\x5F\x45\xAA\x48\xAC'
+import network
+
+zal_stena = b'\xC0\x5D\x89\xB0\x9C\xB8'
+zal_light = b'\x34\x5F\x45\xAA\x48\xAC'
+
+def get_espnow_mac():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    mac = wlan.config('mac')
+    
+    mac_bytes = "b'" + "".join(f"\\x{b:02X}" for b in mac) + "'"
+    return mac_bytes
