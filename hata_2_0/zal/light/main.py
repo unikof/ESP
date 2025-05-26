9acc1b822c784ec7595eb114c6178bcdb39eb229
unikof <unikof@hotmail.com>
2025-05-26 23:41:40 +0300

    3

--- hata_2_0/zal/light/main.py
+++ hata_2_0/zal/light/main.py
@@ -2,7 +2,7 @@ import machine
 import network
 import espnow
 from time import sleep, sleep_ms
-import addr
+from addr import zal_wall
 
 hyphens = "=" * 40 + ">>>"
 #===================================================================
@@ -17,12 +17,17 @@ divan_led.duty(0)
 #===================================================================
 # light statuses, can be: off / on / half
 floor_status = "off" 
-telik_staus = "off"
-divan_staus = "off"
+telik_status = "off"
+divan_status = "off"
 #===================================================================
-print(f"{hyphens}     ZAL LIGHT   ")
+print(f"{hyphens}")
+print("              ZAL LIGHT")
+print(f"{hyphens}")
 #===================================================================
 def control_dag(code):
+    global floor_status, telik_status, divan_status
+    response = ""
+    
     if code == "floor_click":
         if floor_status == "off":
             floor_led.duty(1023)
@@ -30,28 +35,32 @@ def control_dag(code):
         else:
             floor_led.duty(0)
             floor_status = "off"
-        return f"response_{code}"
+        response = f"response_{code}"
     
     elif code == "telik_click":
-        if telik_staus == "off":
+        if telik_status == "off":
             telik_led.duty(1023)
-            telik_staus = "on"
+            telik_status = "on"
         else:
             telik_led.duty(0)
-            telik_staus = "off"
-        return f"response_{code}"
+            telik_status = "off"
+        response = f"response_{code}"
         
     elif code == "divan_click":
-        if divan_staus == "off":
+        if divan_status == "off":
             divan_led.duty(1023)
-            divan_staus = "on"
+            divan_status = "on"
         else:
             divan_led.duty(0)
-            divan_staus = "off"
-        return f"response_{code}"
+            divan_status = "off"
+        response = f"response_{code}"
     
-    else:
-        return f"unknown_command_{code}"
+    else:        
+        response = f"unknown_command_{code}"
+    
+    print(f"statuses <floor/telik/divan> = <{floor_status}/{telik_status}/{divan_status}>")    
+    return response
+
 #===================================================================
 #print("current MAC: ", addr.get_espnow_mac())
 print("standing up WLAN...")
@@ -62,11 +71,12 @@ wlan.active(True)
 esp = espnow.ESPNow()
 esp.active(True)
 
-destination = addr.zal_wall
+destination = zal_wall
 esp.add_peer(destination)
 
 print("receiving...:")
 print(hyphens)
+
 #===================================================================
 while True:
     if esp.any():
@@ -74,6 +84,7 @@ while True:
         code = msg.decode()
         print(f"received <<<<=== {code}")
         response_code = control_dag(code)
+        sleep_ms(10)
         esp.send(destination, response_code)
         print(f"sent ===>>> {response_code}")
         print(hyphens)
