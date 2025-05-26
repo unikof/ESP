46c45381af8c5e7823977936dd8048bad76b0efd
unikof <unikof@hotmail.com>
2025-05-26 17:50:18 +0300

    may26

--- hata_2_0/zal/light/main.py
+++ hata_2_0/zal/light/main.py
@@ -5,44 +5,53 @@ from time import sleep, sleep_ms
 import addr
 
 hyphens = "=" * 40 + ">>>"
+#===================================================================
+#Led control
+floor_led = machine.PWM(machine.Pin(18), freq=10000)
+telik_led = machine.PWM(machine.Pin(19), freq=10000)
+divan_led = machine.PWM(machine.Pin(21), freq=10000)
 
-#LED_1 = 1
-LED_2 = 2
-#LED_3 = 3
-
-#pwm_led_1 = machine.PWM(machine.Pin(LED_1), freq=1000)
-pwm_led_2 = machine.PWM(machine.Pin(LED_2), freq=1000)
-#pwm_led_3 = machine.PWM(machine.Pin(LED_3), freq=1000)
-
-#pwm_led_1.duty(0)
-pwm_led_2.duty(0)
-#pwm_led_3.duty(0)
-
-LIGHT_STATUSES = {
-  "floor": 0,
-  "telik": 0,
-  "divan": 0
-}
+floor_led.duty(0)
+telik_led.duty(0)
+divan_led.duty(0)
+#===================================================================
+# light statuses, can be: off / on / half
+floor_status = "off" 
+telik_staus = "off"
+divan_staus = "off"
 #===================================================================
 print(f"{hyphens}     ZAL LIGHT   ")
 #===================================================================
-def check_code(code):
-    if code in ["floor_click", "floor_longpress", "telik_click", "telik_longpress", "divan_click", "divan_longpress"]:
-        response = f"response_{code}"
-    else:
-        response = f"unknown_command_{code}"
-    return response
-
 def control_dag(code):
+    if code == "floor_click":
+        if floor_status == "off":
+            floor_led.duty(1023)
+            floor_status = "on"
+        else:
+            floor_led.duty(0)
+            floor_status = "off"
+        return f"response_{code}"
     
-    
-    
-    
+    elif code == "telik_click":
+        if telik_staus == "off":
+            telik_led.duty(1023)
+            telik_staus = "on"
+        else:
+            telik_led.duty(0)
+            telik_staus = "off"
+        return f"response_{code}"
         
+    elif code == "divan_click":
+        if divan_staus == "off":
+            divan_led.duty(1023)
+            divan_staus = "on"
+        else:
+            divan_led.duty(0)
+            divan_staus = "off"
+        return f"response_{code}"
     
-   
-        
-    return check_code(code)
+    else:
+        return f"unknown_command_{code}"
 #===================================================================
 #print("current MAC: ", addr.get_espnow_mac())
 print("standing up WLAN...")
@@ -58,38 +67,36 @@ esp.add_peer(destination)
 
 print("receiving...:")
 print(hyphens)
-
+#===================================================================
 while True:
     if esp.any():
         peer, msg = esp.recv()
         code = msg.decode()
         print(f"received <<<<=== {code}")
         response_code = control_dag(code)
-        sleep_ms(10)
         esp.send(destination, response_code)
         print(f"sent ===>>> {response_code}")
         print(hyphens)
-"""
-while True:
-    pwm_led_2.duty(0)
-    sleep_ms(1000)
-    pwm_led_2.duty(1023)
-    sleep_ms(1000)
-    
-while True:
-    for duty in range(0, 50, 1):
-        print(duty)
-        pwm_led_2.duty(duty)
-        sleep_ms(500)
-"""
-
-
-
-
-
-
+#===================================================================
 print(hyphens)
 print("MAIN END...")
 print(hyphens)
+"""
+while True:
+    telik_led.duty(1023)
+    sleep_ms(1000)
+    telik_led.duty(0)
+    sleep_ms(1000)       
+
+
+
+while True:
+    for duty in range(30, 100, 3):
+        print(duty)
+        floor_led.duty(duty)
+        telik_led.duty(duty)
+        divan_led.duty(duty)
+        sleep_ms(500)
+"""
 
 
