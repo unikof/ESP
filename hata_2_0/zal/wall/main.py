7d2542c8b921ea70171b91818f18244a6d6777c7
unikof <unikof@hotmail.com>
2025-05-27 17:01:47 +0300

    27_may

--- hata_2_0/zal/wall/main.py
+++ hata_2_0/zal/wall/main.py
@@ -5,6 +5,9 @@ from addr import zal_light, get_espnow_mac
 import machine
 
 hyphens = "=" * 40 + ">>>"
+button_1 = machine.Pin(18, machine.Pin.IN, machine.Pin.PULL_UP)
+button_2 = machine.Pin(19, machine.Pin.IN, machine.Pin.PULL_UP)
+button_3 = machine.Pin(21, machine.Pin.IN, machine.Pin.PULL_UP)
 #===================================================================
 print(f"{hyphens}")
 print("              ZAL WALL")
@@ -27,6 +30,51 @@ def wait_for_response():
             return msg.decode()
     return "TIME_OUT"
 #===================================================================
+def on_click(button_number):
+    if button_number == 1:
+        send_mess("floor_click")
+    if button_number == 2:
+        send_mess("telik_click")
+    if button_number == 3:
+        send_mess("divan_click")
+
+def on_long_press(button_number):
+    if button_number == 1:
+        send_mess("floor_long_press")
+    if button_number == 2:
+        send_mess("telik_long_press")
+    if button_number == 3:
+        send_mess("divan_long_press")
+    
+    sleep_ms(1000)
+
+def button_pressed(button):
+    if button.value() == 0:
+        return True
+    else:
+        return False
+
+def get_press_type(button, numb):
+    sleep_ms(50) #Tremor
+    current_time = ticks_ms()
+    time_diff = 0
+    
+    while True:
+        time_diff = ticks_diff(ticks_ms(), current_time)
+        if button_pressed(button_1) == False or time_diff >= 400:
+            if time_diff >= 400:
+                #print(time_diff)
+                on_long_press(numb)
+                current_time = 0
+                time_diff = 0
+                break
+            else:
+                #print(time_diff)
+                on_click(numb)
+                current_time = 0
+                time_diff = 0
+                break
+#===================================================================
 print("current MAC: ", get_espnow_mac())
 print("standing up WLAN...")
 
@@ -38,24 +86,30 @@ esp.active(True)
 
 esp.add_peer(zal_light)
 
-print("listening butts...:")
+print("STARTED, listening butts...:")
 print(hyphens)
+    
    
-send_mess("telik_click")
-sleep(1)
-send_mess("telik_click")
-sleep(1)
-send_mess("telik_click")
-sleep(1)
-send_mess("telik_click")
-sleep(1)
-send_mess("telik_click")
-sleep(1)
-send_mess("telik_click")
-sleep(1)
+while True:
+    if button_pressed(button_1) == True:
+        get_press_type(button_1, 1)
+    
+    if button_pressed(button_2) == True:
+        get_press_type(button_2, 2)
+    
+    if button_pressed(button_3) == True:
+        get_press_type(button_3, 3)
+        
+                    
+"""
 
+sleep(1)
+send_mess("telik_click")
+sleep(1)
+"""
 
 print(hyphens)
 print("MAIN END...")
 print(hyphens)
+
 
