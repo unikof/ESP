a919218b0478f88d1acfcd3bd8b942832651fa69
unikof <unikof@hotmail.com>
2025-05-31 14:00:19 +0300

    31may

--- hata_2_0/boot.py
+++ hata_2_0/boot.py
@@ -1,67 +1,96 @@
 import time
+import ntptime
+import machine
 
-hyphens = "=" * 30 + ">>>"
+hyphens = "=" * 40 + ">>>"
 
 print(hyphens)
 print("...BOOT STARTED...")
 
-do_update = False
+do_update = True
 
-if(do_update):
+print(hyphens)
+print("5 seconds pause for cancelling...")
+time.sleep(5)
+print(hyphens)
+
+if do_update:
     print("update activated...")
     
-    import network
+    import func
     import urequests
-    import os
-    import gc
-    import time
-    import socket
-
-    SSID = "HP2_4"
-    PASSWORD = "harrypotter"
-
-    wifi = network.WLAN(network.STA_IF)
-    wifi.active(True)
-    wifi.connect(SSID, PASSWORD)
-
-    while not wifi.isconnected():
-        time.sleep(1)
-
-    print("WiFi OK, network config:", wifi.ifconfig())
-
-    time.sleep(1)
-
-    GD_URL = "https://raw.githubusercontent.com/unikof/ESP/refs/heads/main/Hata/detsk/remote_door/main.py"
-
-    try:
-        print("downloading new_main.py...")
-        response = urequests.get(GD_URL)
-        if response.status_code == 200:
-            with open("main.py", "w") as f:
-                f.write(response.text)
-            print("UPDATE SUCCESS.....")
+    
+    wifi = func.connect_wifi()
+    
+    if wifi:
+        print(hyphens)
+      
+        try:
+            print("downloading new_main.py...")
+            response = urequests.get("https://raw.githubusercontent.com/unikof/ESP/main/hata_2_0/zal/wall/main.py") #PERSONAL FOR EACH
+            if response.status_code == 200:
+                print("response_status: 200")
+                with open("main.py", "w") as f:
+                    f.write(response.text)
+                print("UPDATE MAIN SUCCESS.....")
+                print(hyphens)
+            else:
+                print("downloading failed, status:", response.status_code)
+            response.close()
+        except Exception as e:
+            print("Error:", e)
             print(hyphens)
-        else:
-            print("downloading failed, status:", response.status_code)
-        response.close()
-    except Exception as e:
-        print("Error:", e)
-        print(hyphens)
-    finally:
-        gc.collect()
-        print(hyphens)
-        print("5 seconds pause...")
-        time.sleep(5)
-        print(hyphens)
-        print("starting main NOW...")
-        print(hyphens)
-        import main
+            
+        try:
+            print("downloading new_addr.py...")
+            response = urequests.get("https://raw.githubusercontent.com/unikof/ESP/main/hata_2_0/addr.py")
+            if response.status_code == 200:
+                print("response_status: 200")
+                with open("addr.py", "w") as f:
+                    f.write(response.text)
+                print("UPDATE ADDR SUCCESS.....")
+                print(hyphens)
+            else:
+                print("downloading failed, status:", response.status_code)
+            response.close()
+        except Exception as e:
+            print("Error:", e)
+            print(hyphens)
+            
+        try:
+            print("downloading new_addr.py...")
+            response = urequests.get("https://raw.githubusercontent.com/unikof/ESP/main/hata_2_0/func.py")
+            if response.status_code == 200:
+                print("response_status: 200")
+                with open("func.py", "w") as f:
+                    f.write(response.text)
+                print("UPDATE ADDR SUCCESS.....")
+                print(hyphens)
+            else:
+                print("downloading failed, status:", response.status_code)
+            response.close()
+        except Exception as e:
+            print("Error:", e)
+            print(hyphens)
+        try:        
+            ntptime.host = "pool.ntp.org"
+            ntptime.settime()
+            print("NTP time sync OK")
+        except Exception as e:
+            print("Error:", e)
+            print(hyphens)
+        finally:
+            gc.collect()
+            print("starting main NOW..........")
+            import main
 else:
     gc.collect()
     print(hyphens)
-    print("update SKIPPED, 5 seconds pause...")
-    time.sleep(5)
+    print("update SKIPPED...")
     print(hyphens)
-    print("starting main NOW...")
+    print("starting main NOW..........")
     
     import main
+
+
+
---
+++ hata_2_0/func.py
@@ -0,0 +1,57 @@
+import network
+import os
+import time
+
+hyphens = "=" * 40 + ">>>"
+
+def connect_wifi():
+    
+    ssid = "HP2_4"
+    password = "harrypotter"
+    max_retries = 10
+    wlan = network.WLAN(network.STA_IF)
+    
+    wlan.active(False)
+    time.sleep(1)
+    wlan.active(True)
+    
+    print(hyphens)
+    print(f"Scanning networks...")
+    networks = wlan.scan()
+    found = False
+    for net in networks:
+        net_ssid = net[0].decode('utf-8')
+        print(f"Found: {net_ssid}")
+        if net_ssid == ssid:
+            found = True
+    
+    if not found:
+        print(f"{ssid} not found!")
+        return False
+    
+    print(f"Connecting to {ssid}...")
+    wlan.connect(ssid, password)
+    
+    print(hyphens)
+    
+    retries = 0
+    while retries < max_retries:
+        status = wlan.status()
+        if status == network.STAT_GOT_IP:
+            print("CONNECTED !!!")
+            print(f"IP: {wlan.ifconfig()}")
+            return True
+        elif status == network.STAT_CONNECTING:
+            print("connecting...")
+        elif status == network.STAT_WRONG_PASSWORD:
+            print("Incorrect password!")
+            return False
+        elif status == network.STAT_NO_AP_FOUND:
+            print("Access point not found!")
+            return False
+       
+        time.sleep(1)
+        retries += 1
+    
+    print("Timeout (((.....")
+    return False
